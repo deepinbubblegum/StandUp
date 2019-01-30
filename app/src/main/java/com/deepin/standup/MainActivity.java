ab50f782979b190a48044f9e9ddc325bd33aa771
deepinbubblegum <5911110222@mutacth.com>
2019-01-30 13:41:36 +0700

    commit

--- app/src/main/java/com/deepin/standup/MainActivity.java
+++ app/src/main/java/com/deepin/standup/MainActivity.java
@@ -2,8 +2,12 @@ package com.deepin.standup;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CompoundButton;
 import android.widget.Toast;
@@ -16,6 +20,22 @@ public class MainActivity extends AppCompatActivity {
     private static final String PRIMARY_CHANNEL_ID =
             "primary_notification_channel";
 
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)
+                .setContentTitle("Stand Up Alert")
+                .setContentText("You should stand up and walk around now!")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
         mNotificationManager = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
-
         alarmToggle.setOnCheckedChangeListener(
                 new CompoundButton.OnCheckedChangeListener() {
                     @Override
@@ -32,10 +51,14 @@ public class MainActivity extends AppCompatActivity {
                                                  boolean isChecked) {
                         String toastMessage;
                         if(isChecked){
-                            //Set the toast message for the "on" case.
+                            deliverNotification(MainActivity.this);
+                            //Set the toast message for the "on" case
                             toastMessage = "Stand Up Alarm On!";
                         } else {
-                            //Set the toast message for the "off" case.
+                            //Cancel notification if the alarm is turned off
+                            mNotificationManager.cancelAll();
+
+                            //Set the toast message for the "off" case
                             toastMessage = "Stand Up Alarm Off!";
                         }
 
